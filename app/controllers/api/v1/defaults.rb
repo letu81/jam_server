7f540ba348e128eba4f79fb56497335e2ffe2947
Richard Hessler <richessler@gmail.com>
2016-05-04 23:59:55 -0400

    Adding Routes and defaults

---
+++ app/controllers/api/v1/defaults.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+module API
+  module V1
+    module Defaults
+      extend ActiveSupport::Concern
+
+      included do
+        helpers do
+          # will use with auth
+          # def current_user
+          #   resource_owner
+          # end
+
+          def present_model(model, with:)
+            if model.valid?
+              present model, with: with
+            else
+              error!({ message: 'Validation error', errors: model.errors.full_messages }, 422)
+            end
+          end
+        end
+      end
+
+      def auth_header(description)
+        {
+            Authorization: {
+                description: description,
+                required: true
+            }
+        }
+      end
+
+      def auth_headers
+        auth_header('The authorization token.')
+      end
+
+      def client_auth_headers
+        auth_header('The client authorization token.')
+      end
+
+      module_function :auth_headers, :client_auth_headers, :auth_header
+    end
+  end
+end
