42b2b03a320edf7bd74b912da37e1a1d7600b008
runtropolis <matt3r@gmail.com>
2016-05-09 11:50:36 -0400

    venue entity descriptions

--- app/controllers/api/v1/entities/venue.rb
+++ app/controllers/api/v1/entities/venue.rb
@@ -7,29 +7,22 @@ module API
 
         format_with(:iso_timestamp, &:iso8601)
 
-        expose :venue_id, documentation: {type: String, desc: 'brief desc here'}
-        expose :added_manually, documentation: {type: String, desc: 'brief desc here'}
-        expose :address, documentation: {type: String, desc: 'brief desc here'}
-        expose :blurb, documentation: {type: String, desc: 'brief desc here'}
-        expose :city, documentation: {type: String, desc: 'brief desc here'}
-        expose :facebook, documentation: {type: String, desc: 'brief desc here'}
-        expose :image, documentation: {type: String, desc: 'brief desc here'}
-        expose :lat, documentation: {type: String, desc: 'brief desc here'}
-        expose :lon, documentation: {type: String, desc: 'brief desc here'}
-        expose :location, documentation: {type: String, desc: 'brief desc here'}
-        expose :name, documentation: {type: String, desc: 'brief desc here'}
-        expose :score, documentation: {type: String, desc: 'brief desc here'}
-        expose :slug, documentation: {type: String, desc: 'brief desc here'}
-        expose :state, documentation: {type: String, desc: 'brief desc here'}
-        expose :twitter, documentation: {type: String, desc: 'brief desc here'}
-        expose :website, documentation: {type: String, desc: 'brief desc here'}
-
-         # expose :web_description, documentation: {type: String, desc: 'brief desc here'}
-
-        # with_options(format_with: :iso_timestamp) do
-        #   expose :created_at
-        #   expose :updated_at
-        # end
+        expose :venue_id, documentation: {type: String, desc: 'unique id auto-generated from ticketfly or form'}
+        expose :added_manually, documentation: {type: String, desc: 'if venue is added by user'}
+        expose :address, documentation: {type: String, desc: 'street address'}
+        expose :blurb, documentation: {type: String, desc: 'brief description of venue'}
+        expose :city, documentation: {type: String, desc: ''}
+        expose :facebook, documentation: {type: String, desc: 'venue facebook url'}
+        expose :image, documentation: {type: String, desc: 'image of venue'}
+        expose :lat, documentation: {type: String, desc: 'latitude'}
+        expose :lon, documentation: {type: String, desc: 'longitude'}
+        expose :location, documentation: {type: String, desc: 'location (ex: 9th & V)'}
+        expose :name, documentation: {type: String, desc: 'venue name'}
+        expose :score, documentation: {type: String, desc: 'venue popularity score'}
+        expose :slug, documentation: {type: String, desc: ''}
+        expose :state, documentation: {type: String, desc: 'DC MD or VA'}
+        expose :twitter, documentation: {type: String, desc: 'venue twitter url'}
+        expose :website, documentation: {type: String, desc: 'venue website url'}
 
         private
 
