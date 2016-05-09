6695764dddad7c3e43bb28c86f5de18d275558c9
runtropolis <matt3r@gmail.com>
2016-05-09 00:51:01 -0400

    add truck field descriptions

--- app/controllers/api/v1/entities/truck_event.rb
+++ app/controllers/api/v1/entities/truck_event.rb
@@ -7,38 +7,33 @@ module API
 
         format_with(:iso_timestamp, &:iso8601)
 
-        expose :city, documentation: {type: String, desc: 'brief desc here'}
-        expose :date_date, documentation: {type: String, desc: 'brief desc here'}
-        expose :day_of_week, documentation: {type: String, desc: 'brief desc here'}
-        expose :dateMD, documentation: {type: String, desc: 'brief desc here'}
-        expose :description, documentation: {type: String, desc: 'brief desc here'}
-        expose :end_time, documentation: {type: String, desc: 'brief desc here'}
-        expose :end_time_text, documentation: {type: String, desc: 'brief desc here'}
-        expose :event, documentation: {type: String, desc: 'brief desc here'}
-        expose :hashtag, documentation: {type: String, desc: 'brief desc here'}
-        expose :hasFree, documentation: {type: String, desc: 'brief desc here'}
-        expose :hasEvent, documentation: {type: String, desc: 'brief desc here'}
-        expose :hasSpecial, documentation: {type: String, desc: 'brief desc here'}
-        expose :image, documentation: {type: String, desc: 'brief desc here'}
-        expose :isFeatured, documentation: {type: String, desc: 'brief desc here'}
-        expose :landmark, documentation: {type: String, desc: 'brief desc here'}
-        expose :latitude, documentation: {type: String, desc: 'brief desc here'}
-        expose :location, documentation: {type: String, desc: 'brief desc here'}
-        expose :longitude, documentation: {type: String, desc: 'brief desc here'}
-        expose :metro, documentation: {type: String, desc: 'brief desc here'}
-        expose :neighborhood, documentation: {type: String, desc: 'brief desc here'}
-        expose :neighborhood_id, documentation: {type: String, desc: 'brief desc here'}
-        expose :slug, documentation: {type: String, desc: 'brief desc here'}
-        expose :start_time, documentation: {type: String, desc: 'brief desc here'}
-        expose :start_time_text, documentation: {type: String, desc: 'brief desc here'}
-        expose :state, documentation: {type: String, desc: 'brief desc here'}
-        expose :truck_name, documentation: {type: String, desc: 'brief desc here'}
-        expose :vendor_id, documentation: {type: String, desc: 'brief desc here'}
-
-        # with_options(format_with: :iso_timestamp) do
-        #   expose :created_at
-        #   expose :updated_at
-        # end
+        expose :city, documentation: {type: String, desc: ''}
+        expose :date_date, documentation: {type: String, desc: 'date formatted'}
+        expose :day_of_week, documentation: {type: String, desc: 'day of week'}
+        expose :dateMD, documentation: {type: String, desc: 'date'}
+        expose :description, documentation: {type: String, desc: 'truck description'}
+        expose :end_time, documentation: {type: String, desc: 'end time in time format'}
+        expose :end_time_text, documentation: {type: String, desc: 'end time as string'}
+        expose :event, documentation: {type: String, desc: 'event hosting food truck (ex: Truckaroo)'}
+        expose :hashtag, documentation: {type: String, desc: ''}
+        expose :hasFree, documentation: {type: String, desc: 'truck has a free item. bool.'}
+        expose :hasEvent, documentation: {type: String, desc: 'truck event is at an event. bool.'}
+        expose :hasSpecial, documentation: {type: String, desc: 'truck has a special. bool.'}
+        expose :image, documentation: {type: String, desc: 'image of truck at truck event'}
+        expose :isFeatured, documentation: {type: String, desc: 'truck is featured. bool.'}
+        expose :landmark, documentation: {type: String, desc: 'landmark near truck event'}
+        expose :latitude, documentation: {type: String, desc: 'lat'}
+        expose :location, documentation: {type: String, desc: 'location of truck (ex: 16th & K)'}
+        expose :longitude, documentation: {type: String, desc: 'lon'}
+        expose :metro, documentation: {type: String, desc: 'nearest metro station'}
+        expose :neighborhood, documentation: {type: String, desc: 'nearest neighborhood'}
+        expose :neighborhood_id, documentation: {type: String, desc: 'id of neighborhood'}
+        expose :slug, documentation: {type: String, desc: ''}
+        expose :start_time, documentation: {type: String, desc: 'start time in time format'}
+        expose :start_time_text, documentation: {type: String, desc: 'start time as string'}
+        expose :state, documentation: {type: String, desc: 'DC MD or VA'}
+        expose :truck_name, documentation: {type: String, desc: 'truck name from truck_vendor'}
+        expose :vendor_id, documentation: {type: String, desc: 'vendor id from truck_vendor'}
 
         private
 
--- app/controllers/api/v1/entities/truck_vendor.rb
+++ app/controllers/api/v1/entities/truck_vendor.rb
@@ -7,31 +7,26 @@ module API
 
         format_with(:iso_timestamp, &:iso8601)
 
-        expose :description, documentation: {type: String, desc: 'brief desc here'}
-        expose :facebook, documentation: {type: String, desc: 'brief desc here'}
-        expose :hashtag, documentation: {type: String, desc: 'brief desc here'}
-        expose :image, documentation: {type: String, desc: 'brief desc here'}
-        expose :instagram, documentation: {type: String, desc: 'brief desc here'}
-        expose :logo, documentation: {type: String, desc: 'brief desc here'}
-        expose :menu, documentation: {type: String, desc: 'brief desc here'}
-        expose :name, documentation: {type: String, desc: 'brief desc here'}
-        expose :performance_score, documentation: {type: String, desc: 'brief desc here'}
-        expose :phone_number, documentation: {type: String, desc: 'brief desc here'}
-        expose :price_score, documentation: {type: String, desc: 'brief desc here'}
-        expose :primary_category, documentation: {type: String, desc: 'brief desc here'}
-        expose :secondary_category, documentation: {type: String, desc: 'brief desc here'}
-        expose :slug, documentation: {type: String, desc: 'brief desc here'}
-        expose :twitter, documentation: {type: String, desc: 'brief desc here'}
-        expose :url, documentation: {type: String, desc: 'brief desc here'}
-        expose :contact_name, documentation: {type: String, desc: 'brief desc here'}
-        expose :contact_email, documentation: {type: String, desc: 'brief desc here'}
-        expose :contact_phone, documentation: {type: String, desc: 'brief desc here'}
-        expose :isFeatured, documentation: {type: String, desc: 'brief desc here'}
-
-        # with_options(format_with: :iso_timestamp) do
-        #   expose :created_at
-        #   expose :updated_at
-        # end
+        expose :description, documentation: {type: String, desc: 'truck vendor description'}
+        expose :facebook, documentation: {type: String, desc: 'truck facebook full url'}
+        expose :hashtag, documentation: {type: String, desc: 'truck hashtag custom field'}
+        expose :image, documentation: {type: String, desc: 'image of truck'}
+        expose :instagram, documentation: {type: String, desc: 'truck vendor instagram full url'}
+        expose :logo, documentation: {type: String, desc: 'truck logo'}
+        expose :menu, documentation: {type: String, desc: 'truck menu longtext'}
+        expose :name, documentation: {type: String, desc: 'truck name'}
+        expose :performance_score, documentation: {type: String, desc: 'user rating of truck'}
+        expose :phone_number, documentation: {type: String, desc: 'truck phone number'}
+        expose :price_score, documentation: {type: String, desc: 'user rating of price point'}
+        expose :primary_category, documentation: {type: String, desc: 'primary category or cuisine of food (ie: American)'}
+        expose :secondary_category, documentation: {type: String, desc: 'specialty cuisine (ie: Burgers)'}
+        expose :slug, documentation: {type: String, desc: 'url slug'}
+        expose :twitter, documentation: {type: String, desc: 'truck twitter full url'}
+        expose :url, documentation: {type: String, desc: 'website url'}
+        expose :contact_name, documentation: {type: String, desc: 'truck contact name'}
+        expose :contact_email, documentation: {type: String, desc: 'truck contact email'}
+        expose :contact_phone, documentation: {type: String, desc: 'truck contact phone number'}
+        expose :isFeatured, documentation: {type: String, desc: 'truck is featured'}
 
         private
 
