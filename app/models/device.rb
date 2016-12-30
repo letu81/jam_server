4967a354605fd38287971fa74ac14d470a3eb271
letu81 <1756991323@qq.com>
2016-12-30 13:40:07 +0800

    update device uuid

--- app/models/device.rb
+++ app/models/device.rb
@@ -7,7 +7,7 @@ class Device < ActiveRecord::Base
 	def self.by_device(device_id)
 		self.joins("INNER JOIN device_uuids ON device_uuids.id = devices.uuid")
 		.where(:id => device_id)
-		.select("devices.id, devices.name, devices.mac, devices.status_id, device_uuids.uuid, device_uuids.password")
+		.select("devices.id, devices.name, devices.mac, devices.status_id, device_uuids.uuid as dev_uuid, device_uuids.password")
 		.first
 	end
 
@@ -15,7 +15,7 @@ class Device < ActiveRecord::Base
 		self.joins("INNER JOIN user_devices ON user_devices.device_id = devices.id 
 			        INNER JOIN device_uuids ON device_uuids.id = devices.uuid")
 		.where(:user_devices => {:user_id => user_id})
-		.select("devices.id, devices.name, devices.mac, devices.status_id, device_uuids.uuid, device_uuids.password")
+		.select("devices.id, devices.name, devices.mac, devices.status_id, device_uuids.uuid as dev_uuid, device_uuids.password")
 	end
 
 	def self.by_device_mac_pwd(mac, pwd)
