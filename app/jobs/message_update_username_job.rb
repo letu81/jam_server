da6c27b445f780e0c68073b5034a2b163002f72f
letu81 <1756991323@qq.com>
2017-03-27 17:36:21 +0800

    add lock dir

--- app/jobs/message_update_username_job.rb
+++ app/jobs/message_update_username_job.rb
@@ -6,12 +6,12 @@ class MessageUpdateUsernameJob < ActiveJob::Base
     	message = args[0]
     	user = DeviceUser.where(device_id: message.device_id, device_type: message.lock_type, device_num: message.device_num).first
     	if user
-    		message.update_attribute(username: user.name)
+    		message.update_attribute(:username, user.name)
     	else
     		types = {1 => "指纹", 2 => "密码", 3 => "IC卡"}
-    		message.update_attribute(username: "##{message.device_num}")
+    		message.update_attribute(:username, "##{message.device_num}#{types[message.device_num]}")
     	end
-    else
+    rescue Exception => e
         p "MessageUpdateUsernameJob error...."
         p e.message
     end
