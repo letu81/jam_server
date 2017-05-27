03daa096ee63ce8cbdb34b96f1784d84d282bd5f
letu81 <1756991323@qq.com>
2017-05-27 16:43:43 +0800

    update swagger

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -1,4 +1,4 @@
 GrapeSwaggerRails.options.url = '/api/v1/doc'
 GrapeSwaggerRails.options.before_filter_proc = proc {
-  GrapeSwaggerRails.options.app_url = 'http://192.168.0.105:3000'
+  GrapeSwaggerRails.options.app_url = 'http://0.0.0.0:3000'
 }
